f0da1f9e0da2b4edaf55524ad558ad2a9ffc4a51
Nahid Hasan <nahidn228@gmail.com>
2025-04-14 20:58:15 +0600

    server setup done

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 5000
+require("dotenv").config();
 
-app.get('/', (req, res) => {
-  res.send('Nime is Runnnig!')
-})
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 5000;
+
+
+app.get("test-ai", (req, res) => {});
+
+app.get("/", (req, res) => {
+  res.send({ msg: "Lets crack The power of Nime!" });
+});
 
 app.listen(port, () => {
-  console.log(`Nime app listening on port ${port}`)
-})
+  console.log(`Nime app listening on port ${port}`);
+});
