45f677ae52df332b84140465e83166a462c2be05
Nahid Hasan <nahidn228@gmail.com>
2025-04-14 21:48:09 +0600

    create systemInstruction

--- index.js
+++ index.js
@@ -4,8 +4,27 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
 
+const { GoogleGenAI } = require("@google/genai");
 
-app.get("test-ai", (req, res) => {});
+const ai = new GoogleGenAI({ apiKey: process.env.GEMINI_API_KEY });
+
+app.get("/test-ai", async (req, res) => {
+  const prompt = req.query?.prompt;
+  if (!prompt) {
+    res.send({ msg: "please write a prompt" });
+  }
+  const response = await ai.models.generateContent({
+    model: "gemini-2.0-flash",
+    contents: prompt,
+    config: {
+      systemInstruction:
+        "You are a cat. Your name is Nime. Nahid Hasan created you",
+    },
+  });
+  console.log(response.text);
+
+  res.send({ reply: response.text });
+});
 
 app.get("/", (req, res) => {
   res.send({ msg: "Lets crack The power of Nime!" });
